0606bb053077db1cf01c055bc86ad3593a7d8424
Vaios Kalpias Ilias <vkalpias@gmail.com>
2021-09-23 16:49:33 +0300

    more guid tests

--- test/api/test-guid.js
+++ test/api/test-guid.js
@@ -5,4 +5,10 @@ describe('guid tests', function () {
         expect(guid.length).to.equal(36);
         expect(/^[a-z0-9]{8}-[a-z0-9]{4}-4[a-z0-9]{3}-[a-z0-9]{4}-[a-z0-9]{12}$/.test(guid)).to.equal(true);
     });
+
+    it('creates different guids', function () {
+        const guid1 = api.Guid.create();
+        const guid2 = api.Guid.create();
+        expect(guid1).to.not.equal(guid2);
+    });
 });
