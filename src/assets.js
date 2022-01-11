d931adc1c2b3f494c4bb381aed389e271c927b01
Donovan Hutchence <slimbuck7@gmail.com>
2022-01-11 12:20:50 +0000

    Progress event fix (#37)
    * fix progress
    * also handle loadAll

--- src/assets.js
+++ src/assets.js
@@ -361,10 +361,11 @@ class Assets extends Events {
 
         const onProgress = () => {
             loaded++;
-            this.emit('load:progress', (loaded / total) * 0.5 + 0.5);
             if (loaded === total) {
                 this.emit('load:progress', 1);
                 this.emit('load:all');
+            } else {
+                this.emit('load:progress', (loaded / total) * 0.5 + 0.5);
             }
         };
 
@@ -415,10 +416,11 @@ class Assets extends Events {
 
         const onProgress = () => {
             loaded++;
-            this.emit('load:progress', (loaded / total) * 0.5 + 0.5);
             if (loaded === total) {
                 this.emit('load:progress', 1);
                 this.emit('load:all');
+            } else {
+                this.emit('load:progress', (loaded / total) * 0.5 + 0.5);
             }
         };
 
