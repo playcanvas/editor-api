9fc80b645d30ff97b5da611f94b11ca4e1a6d9f8
kpal <kpal81xd@gmail.com>
2025-01-14 16:40:58 +0000

    Made type import more specific for event handle

--- src/entities/delete.ts
+++ src/entities/delete.ts
@@ -1,4 +1,4 @@
-import type { EventHandle } from '@playcanvas/observer/types';
+import type { EventHandle } from '@playcanvas/observer/types/event-handle';
 
 import { findEntityReferencesInComponents, updateReferences } from './references';
 import { Entity } from '../entity';
