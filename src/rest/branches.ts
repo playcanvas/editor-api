4b3fb9e1b4e57e1a702f5865f1d0ee4b4bda5862
kpal <kpal81xd@gmail.com>
2025-04-08 12:21:08 +0100

    Converted branches

---
+++ src/rest/branches.ts
@@ -0,0 +1,130 @@
+import { Ajax } from '../ajax';
+
+export type BranchCheckpointOptions = {
+    /**
+     * The type of task to get checkpoints for
+     */
+    task_type?: string;
+
+    /**
+     * The maximum number of checkpoints to get
+     */
+    limit?: number;
+
+    /**
+     * The number of checkpoints to skip
+     */
+    skip?: number;
+
+    /**
+     * The ID of the graph to start from
+     */
+    graphStartId?: string;
+
+    /**
+     * The ID of the VC history item
+     */
+    vcHistItem?: string;
+};
+
+/**
+ * Creates a new branch
+ *
+ * @param data - The data for the new branch
+ * @returns A request that responds with the result of the branch creation
+ */
+export const branchCreate = (data: object) => {
+    return Ajax.post({
+        url: '/api/branches',
+        auth: true,
+        data
+    });
+};
+
+/**
+ * Checks out the master branch
+ *
+ * @param branchId - The ID of the branch to check out
+ * @returns A request that responds when the branch is checked out
+ */
+export const branchCheckout = (branchId: string) => {
+    return Ajax.post({
+        url: `/api/branches/${branchId}/checkout`,
+        auth: true
+    });
+};
+
+/**
+ * Opens a branch
+ *
+ * @param branchId - The ID of the branch to open
+ * @returns A request that responds when the branch is opened
+ */
+export const branchOpen = (branchId: string) => {
+    return Ajax.post({
+        url: `/api/branches/${branchId}/open`,
+        auth: true
+    });
+};
+
+/**
+ * Closes a branch
+ *
+ * @param branchId - The ID of the branch to close
+ * @returns A request that responds when the branch is closed
+ */
+export const branchClose = (branchId: string) => {
+    return Ajax.post({
+        url: `/api/branches/${branchId}/close`,
+        auth: true
+    });
+};
+
+/**
+ * Deletes a branch
+ *
+ * @param branchId - The ID of the branch to delete
+ * @returns A request that responds when the branch is deleted
+ */
+export const branchDelete = (branchId: string) => {
+    return Ajax.delete({
+        url: `/api/branches/${branchId}`,
+        auth: true
+    });
+};
+
+/**
+ * Get branch checkpoints
+ *
+ * @param branchId - The ID of the branch to get checkpoints for
+ * @param options - The options for the request
+ * @returns A request that responds with the checkpoints for the branch
+ */
+export const branchCheckpoints = (branchId: string, options: BranchCheckpointOptions) => {
+    const params = [];
+
+    if (options.task_type) {
+        params.push(`task_type=${options.task_type}`);
+    }
+
+    if (options.limit) {
+        params.push(`limit=${options.limit}`);
+    }
+
+    if (options.skip) {
+        params.push(`skip=${options.skip}`);
+    }
+
+    if (options.graphStartId) {
+        params.push(`graphStartId=${options.graphStartId}`);
+    }
+
+    if (options.vcHistItem) {
+        params.push(`vcHistItem=${options.vcHistItem}`);
+    }
+
+    return Ajax.get({
+        url: `/api/branches/${branchId}/checkpoints?${params.join('&')}`,
+        auth: true
+    });
+};
