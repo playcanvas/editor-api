928bc17e8dde06b56639ed77bc309b9e2b790a33
kpal <kpal81xd@gmail.com>
2025-07-16 13:30:05 +0100

    Added types to REST jobs call

--- src/rest/jobs.ts
+++ src/rest/jobs.ts
@@ -1,15 +1,24 @@
 import { Ajax } from '../ajax';
 import { globals as api } from '../globals';
+import type { Job } from '../models';
+
+// requests
+type JobGetRequest = {
+    /**
+     * The ID of the job to get
+     */
+    jobId: number;
+};
+
+// responses
+type JobGetResponse = Job<object>
 
 /**
  * Gets the job with the given ID
- *
- * @param jobId - The ID of the job to get
- * @returns A request that responds with the job data
  */
-export const jobGet = (jobId: number) => {
-    return Ajax.get({
-        url: `${api.apiUrl}/jobs/${jobId}`,
+export const jobGet = (args: JobGetRequest) => {
+    return Ajax.get<JobGetResponse>({
+        url: `${api.apiUrl}/jobs/${args.jobId}`,
         auth: true
     });
 };
