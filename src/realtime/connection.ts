549f1e2ca99ca8cc9e04a19281dee002ac1d70d9
kpal <kpal81xd@gmail.com>
2025-11-20 17:26:58 +0000

    refactor: unify connection state management using a single state variable

--- src/realtime/connection.ts
+++ src/realtime/connection.ts
@@ -34,9 +34,7 @@ class RealtimeConnection extends Events {
 
     private _reconnectInterval: number = RECONNECT_INTERVAL;
 
-    private _connecting: boolean = false;
-
-    private _connected: boolean = false;
+    private _state: 'connecting' | 'connected' | 'disconnected' = 'disconnected';
 
     private _authenticated: boolean = false;
 
@@ -59,7 +57,7 @@ class RealtimeConnection extends Events {
             if (document.hidden) {
                 return;
             }
-            if (!this.connected && this._url) {
+            if (this._state === 'disconnected' && this._url) {
                 this.connect(this._url);
             }
         };
@@ -144,13 +142,13 @@ class RealtimeConnection extends Events {
      * @param url - The server URL
      */
     connect(url: string) {
-        if (this._connecting) {
+        if (this._state === 'connecting') {
             console.warn('already connecting to realtime server');
             return;
         }
 
         this._url = url;
-        this._connecting = true;
+        this._state = 'connecting';
 
         if (this._reconnectAttempts > MAX_ATTEMPTS) {
             this._realtime.emit('cannotConnect');
@@ -164,8 +162,7 @@ class RealtimeConnection extends Events {
         const socket = new WebSocket(url);
 
         socket.addEventListener('open', () => {
-            this._connecting = false;
-            this._connected = true;
+            this._state = 'connected';
             this._reconnectAttempts = 0;
 
             socket.send(`auth${JSON.stringify({ accessToken: api.accessToken })}`);
@@ -194,8 +191,7 @@ class RealtimeConnection extends Events {
                 this._alive = null;
             }
 
-            this._connecting = false;
-            this._connected = false;
+            this._state = 'disconnected';
             this._authenticated = false;
 
             this._realtime.emit('disconnect', reason);
@@ -274,7 +270,7 @@ class RealtimeConnection extends Events {
      * Whether the user is connected to the server
      */
     get connected() {
-        return this._connected;
+        return this._state === 'connected';
     }
 
     /**
